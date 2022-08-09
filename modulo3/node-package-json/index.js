add63782389e9a14302dc9cde88a36a1e00abb77
raiane <raiane.marce@gmail.com>
2022-08-09 20:46:16 -0300

    node-package-json

---
+++ modulo3/node-package-json/index.js
@@ -0,0 +1,13 @@
+// # 1 Exercício 
+// a) Acessamos argumentos passados aos nossos scripts
+// através de process.argv
+
+// b)
+
+// console.log("Olá,", process.argv[2],"! Você tem ", process.argv[3],"anos.")
+
+// c)
+//    const valor = Number(valorEmString)
+//    console.log("Olá,", process.argv[2],"! Você tem ", process.argv[3],"anos. Em sete anos você terá", process.argv[4], "anos.")
+
+//    #2 Exercício 
